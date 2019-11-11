4189f9d0529414d7c02cca82e77c540afcbd27c3
ginpei <ginpei@gmail.com>
2019-11-11 00:32:09 -0800

    update ESLint to support TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,22 @@
-// npm install --save-dev eslint eslint-config-airbnb-base eslint-plugin-import
+// Need following packages beside typescript
+// eslint eslint-config-airbnb-base eslint-plugin-import @typescript-eslint/parser @typescript-eslint/eslint-plugin
 
 module.exports = {
-	"env": {
-		"browser": true,
-		"es6": true
-	},
-	"extends": "eslint-config-airbnb-base",
+  "extends": [
+    "eslint-config-airbnb-base",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  "env": {
+    "browser": true,
+    "es6": true,
+  },
+  "parser": "@typescript-eslint/parser",
 	"rules": {
-    "arrow-parens": [
-      "error",
-      "always"
-    ],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-unused-vars": "off", // leave it to tsc
+    "@typescript-eslint/no-use-before-define": "off",
+    "arrow-parens": ["error", "always"],
     "class-methods-use-this": "off",
-    "import/extensions": [
-      "error",
-      "ignorePackages"
-    ],
-    "space-before-function-paren": [
-      "error",
-      "always"
-    ],
-    "no-unused-vars": [
-      "off"
-    ],
+    "import/prefer-default-export": "off",
   },
 };
