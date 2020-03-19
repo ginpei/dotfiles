9d669fc48d0cad0755709b56a67611d1414d8c4e
ginpei <ginpei@gmail.com>
2020-03-19 11:09:40 -0700

    update default project settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,14 +9,23 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
+    "jest": true,
   },
   "parser": "@typescript-eslint/parser",
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 	"rules": {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-unused-vars": "off", // leave it to tsc
     "@typescript-eslint/no-use-before-define": "off",
     "arrow-parens": ["error", "always"],
     "class-methods-use-this": "off",
+    "import/extensions": ["error", "never"],
     "import/prefer-default-export": "off",
   },
 };
