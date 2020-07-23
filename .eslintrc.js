36889c505f85ca8a246353c15d58075c18584fd3
ginpei <ginpei@gmail.com>
2020-07-23 00:01:18 -0700

    add prettier to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,37 @@
 // Need following packages beside typescript
-// eslint eslint-config-airbnb-base eslint-plugin-import @typescript-eslint/parser @typescript-eslint/eslint-plugin
+// eslint eslint-config-airbnb-base eslint-plugin-import @typescript-eslint/parser @typescript-eslint/eslint-plugin prettier eslint-config-prettier eslint-plugin-prettier
 
 module.exports = {
-  "extends": [
+  extends: [
     "eslint-config-airbnb-base",
     "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest": true,
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
   },
-  "parser": "@typescript-eslint/parser",
+  parser: "@typescript-eslint/parser",
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
       },
     },
   },
-	"rules": {
+  plugins: ["prettier"],
+  rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-unused-vars": "off", // leave it to tsc
     "@typescript-eslint/no-use-before-define": "off",
     "arrow-parens": ["error", "always"],
     "class-methods-use-this": "off",
     "import/extensions": ["error", "never"],
+    "import/no-extraneous-dependencies": [
+      "error",
+      { devDependencies: ["**/*.test.{ts,tsx}"] },
+    ],
     "import/prefer-default-export": "off",
   },
 };
