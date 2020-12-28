954789d1133bc14bb5687dea4cfc300a74ff3c20
ginpei <ginpei@gmail.com>
2020-12-28 00:14:35 -0800

    update to use share ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,9 @@
-// Need following packages beside typescript
-// eslint eslint-config-airbnb-base eslint-plugin-import @typescript-eslint/parser @typescript-eslint/eslint-plugin prettier eslint-config-prettier eslint-plugin-prettier
+// $ npm i -D @ginpei/eslintrc
+// $ npm i -D @typescript-eslint/eslint-plugin eslint eslint-config-prettier eslint-plugin-import eslint-plugin-prettier prettier
 
 module.exports = {
-  extends: [
-    "eslint-config-airbnb-base",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  env: {
-    browser: true,
-    es6: true,
-    jest: true,
-  },
-  parser: "@typescript-eslint/parser",
-  settings: {
-    "import/resolver": {
-      node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
-    },
-  },
-  plugins: ["prettier"],
+  extends: "./node_modules/@ginpei/eslintrc/.eslintrc.js",
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-unused-vars": "off", // leave it to tsc
-    "@typescript-eslint/no-use-before-define": "off",
-    "arrow-parens": ["error", "always"],
-    "class-methods-use-this": "off",
-    "import/extensions": ["error", "never"],
-    "import/no-extraneous-dependencies": [
-      "error",
-      { devDependencies: ["**/*.test.{ts,tsx}"] },
-    ],
-    "import/prefer-default-export": "off",
+    // your rules here
   },
 };
