59b055be64f2e1438900139f01ac2e5229f86985
Ginpei <ginpei@ginpei.dev>
2021-08-11 15:22:15 -0700

    reduce input

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
-// $ npm i -D @ginpei/eslintrc
-// $ npm i -D @typescript-eslint/eslint-plugin eslint eslint-config-prettier eslint-plugin-import eslint-plugin-prettier prettier
+// $ npm i -D @ginpei/eslintrc @typescript-eslint/eslint-plugin eslint eslint-config-prettier eslint-plugin-import eslint-plugin-prettier prettier
 
 module.exports = {
   extends: "./node_modules/@ginpei/eslintrc/.eslintrc.js",
